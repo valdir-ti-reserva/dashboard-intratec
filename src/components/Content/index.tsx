a7c9662f49a2877a01f81ea584fe673b65e6f74d
Valdir Silva <valdir.ti@gmail.com>
2022-06-07 23:25:16 -0300

    feat: split css

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,37 +3,6 @@ import './styles.scss'
 const Content = () => {
   return (
     <main className="main">
-    <div className="main-header">
-      <div className="main-header__intro-wrapper">
-        <div className="main-header__welcome">
-          <div className="main-header__welcome-title text-light">Welcome, <strong>Valdir</strong></div>
-          <div className="main-header__welcome-subtitle text-light">How are you today?</div>
-        </div>
-        <div className="quickview">
-          <div className="quickview__item">
-            <div className="quickview__item-total">41</div>
-            <div className="quickview__item-description">
-              <i className="far fa-calendar-alt"></i>
-              <span className="text-light">Events</span>
-            </div>
-          </div>
-          <div className="quickview__item">
-            <div className="quickview__item-total">64</div>
-            <div className="quickview__item-description">
-              <i className="far fa-comment"></i>
-              <span className="text-light">Messages</span>
-            </div>
-          </div>
-          <div className="quickview__item">
-            <div className="quickview__item-total">27&deg;</div>
-            <div className="quickview__item-description">
-              <i className="fas fa-map-marker-alt"></i>
-              <span className="text-light">Austin</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     <div className="main-overview">
       <div className="overviewCard">
         <div className="overviewCard-icon overviewCard-icon--document">
